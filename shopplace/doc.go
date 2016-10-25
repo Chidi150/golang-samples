da30e345b341ed99c57f32a7bf9fc3978bb43b65
Chidi Iwuanyanwu <Chidi Iwuanyanwu>
2016-10-25 21:25:09 +0100

    updated25-10-2016

---
+++ shopplace/doc.go
@@ -0,0 +1,6 @@
+// Copyright 2016 Google Inc. All rights reserved.
+// Use of this source code is governed by the Apache 2.0
+// license that can be found in the LICENSE file.
+
+// Package shopplace contains the shopplace database and app configuration, shared by the main app module and the worker module.
+package shopplace
